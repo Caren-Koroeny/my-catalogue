9ad981a97cf52356413cf8cce15a4eaa123500e9
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-30 22:12:53 +0300

    fix linters

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Genre do
 
   describe '#add_item' do
     it 'should add an item to the genre' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label', publish_date: Date.new(2022, 1, 1), on_spotify: true)
+      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
+                             publish_date: Date.new(2022, 1, 1), on_spotify: true)
 
       genre.add_item(album)
 
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe MusicAlbum do
 
   describe '#initialize' do
     it 'should set the properties correctly' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label', publish_date: Date.new(2022, 1, 1), on_spotify: true)
+      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
+                             publish_date: Date.new(2022, 1, 1), on_spotify: true)
 
       expect(album.id).to eq(1)
       expect(album.genre).to eq(genre)
@@ -21,19 +22,22 @@ RSpec.describe MusicAlbum do
 
   describe '#can_be_archived?' do
     it 'should return true if publish_date is older than 10 years and on_spotify is true' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label', publish_date: Date.new(2010, 1, 1), on_spotify: true)
+      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
+                             publish_date: Date.new(2010, 1, 1), on_spotify: true)
 
       expect(album.can_be_archived?).to eq(true)
     end
 
     it 'should return false if publish_date is newer than 10 years' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label', publish_date: Date.new(2022, 1, 1), on_spotify: true)
+      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
+                             publish_date: Date.new(2022, 1, 1), on_spotify: true)
 
       expect(album.can_be_archived?).to eq(false)
     end
 
     it 'should return false if on_spotify is false' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label', publish_date: Date.new(2010, 1, 1), on_spotify: false)
+      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
+                             publish_date: Date.new(2010, 1, 1), on_spotify: false)
 
       expect(album.can_be_archived?).to eq(false)
     end
