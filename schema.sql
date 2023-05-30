a5f5fad120521fc23ffd47b5ec5473f0d8e96adf
Caren-Koroeny <carensiya5@gmail.com>
2023-05-30 09:27:29 +0300

    Added two databases for books and label

--- schema.sql
+++ schema.sql
@@ -8,3 +8,23 @@ CREATE TABLE authors(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
   first_name TEXT NOT NULL,
   Last_name TEXT NOT NULL);
+
+  DROP TABLE IF EXISTS book;
+
+CREATE TABLE book(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publisher TEXT NOT NULL,
+  cover_state VARCHAR(20) NOT NULL,
+  publish_date INT NOT NULL,
+  author VARCHAR(70) NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  archived? BOOLEAN NOT NULL
+);
+
+DROP TABLE IF EXISTS label;
+
+CREATE TABLE label(
+  id INT REFERENCES book(id),
+  title TEXT(70),
+  color VARCHAR
+)
