e20e2addde505ae567c9cd8b2490993827309513
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-30 14:16:37 +0300

    Create a class Genre

---
+++ classes/genre.rb
@@ -0,0 +1,17 @@
+require_relative './item'
+
+class Genre
+    attr_reader :id, :name, :items
+  
+    def initialize(id, name)
+      @id = id
+      @name = name
+      @items = []
+    end
+  
+    def add_item(item)
+      item.genre = self
+      items << item
+    end
+  end
+  
