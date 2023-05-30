ee29ee9e880dd37cf8212ad3cc20ff2d6882f38d
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-30 14:41:11 +0300

    Fix Linters

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,17 +1,16 @@
 require_relative './item'
 
 class Genre
-    attr_reader :id, :name, :items
-  
-    def initialize(id, name)
-      @id = id
-      @name = name
-      @items = []
-    end
-  
-    def add_item(item)
-      item.genre = self
-      items << item
-    end
+  attr_reader :id, :name, :items
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @items = []
   end
-  
+
+  def add_item(item)
+    item.genre = self
+    items << item
+  end
+end
--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,15 +1,15 @@
 require_relative './item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
-  
-    def initialize(id, genre, author, source, label, publish_date, on_spotify)
-      super(id, genre, author, source, label, publish_date)
-      @on_spotify = on_spotify
-    end
-  
-    def can_be_archived?
-      super && on_spotify
-    end
+  attr_accessor :on_spotify
+
+  def initialize(id:, genre:, author:, source:, label:, publish_date:, on_spotify:)
+    super(id, genre, author, source, label, publish_date)
+    @on_spotify = on_spotify
   end
-  
+
+  def can_be_archived?
+    super && on_spotify
+  end
+end
+# rubocop:enable Metrics/ParameterLists
