20f6d84713141d751407c36019eb588789d2d988
Caren-Koroeny <carensiya5@gmail.com>
2023-05-31 20:48:53 +0300

    Added test for music

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,45 +1,21 @@
-require 'date'
-require_relative '../classes/music_album'
-require_relative '../classes/genre'
+require_relative './helper_spec'
 
-RSpec.describe MusicAlbum do
-  let(:genre) { Genre.new(1, 'Rock') }
-
-  describe '#initialize' do
-    it 'should set the properties correctly' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
-                             publish_date: Date.new(2022, 1, 1), on_spotify: true)
-
-      expect(album.id).to eq(1)
-      expect(album.genre).to eq(genre)
-      expect(album.author).to eq('Artist')
-      expect(album.source).to eq('CD')
-      expect(album.label).to eq('Label')
-      expect(album.publish_date).to eq(Date.new(2022, 1, 1))
-      expect(album.on_spotify).to eq(true)
-    end
+describe MusicAlbum do
+  before :each do
+    @music = MusicAlbum.new('Happy Day', '2000', false)
   end
 
-  describe '#can_be_archived?' do
-    it 'should return true if publish_date is older than 10 years and on_spotify is true' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
-                             publish_date: Date.new(2010, 1, 1), on_spotify: true)
-
-      expect(album.can_be_archived?).to eq(true)
+  context 'MusicAlbum class' do
+    it 'Should be an instance of the MusicAlbum class' do
+      expect(@music).to be_an_instance_of(MusicAlbum)
     end
 
-    it 'should return false if publish_date is newer than 10 years' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
-                             publish_date: Date.new(2022, 1, 1), on_spotify: true)
-
-      expect(album.can_be_archived?).to eq(false)
+    it 'Should have three properties' do
+      expect(@music).to have_attributes(name: 'Happy Day', publish_date: '2000', on_spotify: false)
     end
 
-    it 'should return false if on_spotify is false' do
-      album = MusicAlbum.new(id: 1, genre: genre, author: 'Artist', source: 'CD', label: 'Label',
-                             publish_date: Date.new(2010, 1, 1), on_spotify: false)
-
-      expect(album.can_be_archived?).to eq(false)
+    it 'Should return false' do
+      expect(@music.can_be_archived?).to be false
     end
   end
 end
