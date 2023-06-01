48f67e1f8ba467162c3a01712da1c8fd8130b63a
Caren-Koroeny <carensiya5@gmail.com>
2023-06-02 00:31:13 +0300

    Added methods for author,label and genre classes

--- classes/item.rb
+++ classes/item.rb
@@ -7,10 +7,23 @@ class Item
     @archived = archived
   end
 
-  def move_to_achive
-    return unless Item.can_be_archived?
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 
-    @archived = true
+  def add_author(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
   end
 
   private
